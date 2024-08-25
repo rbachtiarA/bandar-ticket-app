dc862d34b32a92a3970986899514f7761084b7c8
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-08-25 22:16:42 +0700

    feat: add Carousel component

--- apps/web/src/components/carousel.tsx
+++ apps/web/src/components/carousel.tsx
@@ -1,26 +1,43 @@
+'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Carousel() {
+
+    const [curr, setCurr] = useState(0)
     const images = [
         '/city/1.jpg',
         '/city/2.webp',
         '/city/3.jpg'
     ]
+
+    useEffect(() => {
+        const slideInterval = setInterval(next, 7000)
+        return () => clearInterval(slideInterval)
+    },[curr])
+
+    const prev = () => setCurr((curr) => (curr === 0? images.length - 1 : curr - 1)) 
+    const next = () => setCurr((curr) => (curr === images.length - 1? 0 : curr + 1)) 
+
   return (
-  <div className=''>    
-        <div className='overflow-hidden'>
-            {images.map((img) => (
-                <Image
-                src={img}
-                alt='city'
-                width={600}
-                height={400}
-                sizes='100vw'
-                />
-      
-            ))}
-        </div>
+  <div className='w-screen mx-auto'>
+      <div className='overflow-hidden relative'>          
+          <div className=''>
+              
+                  <Image        
+                  src={images[curr]}
+                  alt='city'
+                  width={0}
+                  height={0}
+                  sizes='100vw'
+                  style={{width: '100%', height: 'auto'}}
+                  />
+              
+          </div>
+          <button onClick={next}>Next</button>
+          <button onClick={prev}>Prev</button>
+      </div>
+
   </div>
   )
 }
