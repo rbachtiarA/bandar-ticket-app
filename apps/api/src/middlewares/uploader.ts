756de374a9a106346bbe2633014368972450f511
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-08-30 13:56:15 +0700

    feat: add uploader.ts for handle upload file with muler package

---
+++ apps/api/src/middlewares/uploader.ts
@@ -0,0 +1,33 @@
+import path from 'path';
+import multer from 'multer'
+import { Request } from 'express';
+type DestinationCallback = (error: Error | null, destination: string) => void
+type FileNameCallback = (error:Error | null, filename: string) => void
+
+export const uploader = (filePrefix: string, foldername?:string) => {
+    const defaultDir = path.join(__dirname, "../../public")
+
+    const storage = multer.diskStorage({
+        destination: (
+            req:Request,
+            file: Express.Multer.File,
+            cb: DestinationCallback
+        ) => {
+            const destination = foldername? defaultDir + foldername:defaultDir;
+            cb(null, destination)
+        },
+
+        filename: (
+            req:Request,
+            file:Express.Multer.File,
+            cb: FileNameCallback
+        ) => {
+            const originalNameParts = file.originalname.split('.')
+            const fileExtension = originalNameParts[originalNameParts.length - 1]
+            const newFileName = filePrefix+Date.now() + '.' + fileExtension
+            cb(null, newFileName)
+        }
+    })
+
+    return multer({storage: storage})
+}
