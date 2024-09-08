7eecb3e40c74fdf12007f8749898daf6cc58ee6a
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-09-08 21:36:42 +0700

    fix: edit interface for ticketType

--- apps/web/src/app/interfaceType.ts
+++ apps/web/src/app/interfaceType.ts
@@ -24,6 +24,22 @@ export interface IEvent {
     city: ICity
 }
 
+export interface ITicketType {
+    id:number
+    name: string
+    description: string
+    eventId:number
+    price: number
+    quota: number
+}
+
+export interface PostTicketType {
+    ticketName: string
+    ticketDescription: string
+    ticketPrice:number
+    ticketQuota: number
+}
+
 export interface ICity {
     id: number
     name: string
