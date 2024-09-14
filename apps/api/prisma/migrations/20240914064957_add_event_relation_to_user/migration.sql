5db6431f7b6ae41038c921da5d8ef8c1b0134969
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-09-14 14:18:21 +0700

    feat: add relation event and review to user model

---
+++ apps/api/prisma/migrations/20240914064957_add_event_relation_to_user/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `event` ADD COLUMN `userId` INTEGER NULL;
+
+-- AddForeignKey
+ALTER TABLE `Event` ADD CONSTRAINT `Event_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
